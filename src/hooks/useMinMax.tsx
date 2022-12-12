c3300e743e11e95953635d015a2b29522081aea6
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-12-12 18:58:59 +0000

    resolve conflicts

--- src/hooks/useMinMax.tsx
+++ src/hooks/useMinMax.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
 
-export default function useMinMax(data: any, measureType: string): number[] {
-=======
 import {Measurement} from "../models/measurement.model";
 
 export default function useMinMax(data:  Measurement[], measureType: string): number[] {
->>>>>>> 0f10fd24ddec07092eda74e82b0937666ae57e98
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(0);
 
